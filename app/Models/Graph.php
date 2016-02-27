c70d3b58940bbef724f0d29f10c26e51b8ee03e0
Dmitriy Podkorytov <d.podkorytov@mail.ru>
2016-02-28 02:44:19 +0600

    add sort

--- app/Models/Graph.php
+++ app/Models/Graph.php
@@ -2,40 +2,58 @@
 
 namespace App\Models;
 
+use akeinhell\Api2Gis;
+use akeinhell\RequestParams\CarRouteParams;
 use Cache;
 
 class Graph
 {
     /**
      * @param TodoListItem[] $listItems
+     *
      * @return TodoListItem[]
      */
     public static function calculateItemsPoints($listItems = [])
     {
-        return $listItems;
+        return static::sortGraph($listItems);
     }
 
-    protected static function sortGraph(array $listItems)
+    protected static function sortGraph($listItems)
     {
         $initial = [];
         $ways    = [];
-        $cost    = 0;
+        $cost    = 1000000000;
 
         for ($i = 1; $i < count($listItems) - 1; $i++) {
             $initial[] = $i;
         }
 
         while (!static::isDescSort($initial)) {
-            $ways[] = $initial;
+            $ways[]  = $initial;
             $initial = static::getNextPermutation($initial);
         }
         $ways[] = $initial;
 
-        for($i = 0; $i < count($ways); $i++) {
-
+        for ($i = 0; $i < count($ways); $i++) {
+            $way     = array_merge([0], $ways[$i]);
+            $way[]   = (count($listItems) - 1);
+            $wayCost = 0;
+            for ($j = 0; $j < count($way) - 1; $j++) {
+                $edge = static::getEdge($listItems[$way[$i]], $listItems[$way[$i + 1]]);
+                $wayCost += $edge[0]->items[0]->total_distance;
+            }
+            if ($wayCost < $cost) {
+                $cost = $wayCost;
+                $initial = $way;
+            }
         }
 
-        return $ways;
+        $out = [];
+        for ($j = 0; $j < count($way); $j++) {
+            $out[] = $listItems[$initial[$i]];
+        }
+
+        return $out;
     }
 
     protected static function isDescSort($arr)
@@ -89,10 +107,10 @@ class Graph
     {
         $cacheKey = static::getEdgeCacheKey($point1, $point2);
 
-        if ($edge = Cache::get($cacheKey)) {
+        if (!($edge = Cache::get($cacheKey))) {
             $routePoints = new CarRouteParams();
-            $routePoints->addWaypoint($point1);
-            $routePoints->addWaypoint($point2);
+            $routePoints->addWaypoint([$point1->lon, $point1->lat]);
+            $routePoints->addWaypoint([$point2->lon, $point2->lat]);
             Api2Gis::call()->CarRouteDirectionsAsync($routePoints);
             $edge = Api2Gis::call()->execute();
             Cache::put($cacheKey, $edge, 30);
