a44ed99ce4b1e4d5ef6e2d2cb8a536da1c103a0c
Ilya Poluhin <ilyapoluhin@aliendevs.com>
2016-02-27 22:13:06 +0600

    Added model graph.

---
+++ app/Models/Graph.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+class Graph
+{
+    /**
+     * @param \TodoListItems[] $listItems
+     * @return \TodoListItems[]
+     */
+    public static function calculateItemsPoints(array $listItems = [])
+    {
+        return [
+            ["82.910206","55.04982"],
+            ["82.903137","55.060355"],
+            ["82.954886","55.013059"],
+        ];
+    }
+} 
